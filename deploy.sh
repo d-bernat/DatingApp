0232a24ca1bfe5b18beeb4f34d2900c7a723bd26
d-bernat <mail@it-bernat.de>
2020-10-05 13:49:55 +0200

    travis 4

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,2 @@
-docker build -t dbernat/dating-app-api:latest -t dbernat/dating-app-api:$SHA -f ./API/Dockerfile ./API
 docker push dbernat/dating-app-api:latest
 docker push dbernat/dating-app-api:$SHA
